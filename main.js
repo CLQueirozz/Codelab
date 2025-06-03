2938cef00d11d3b7fa238cd3830ea8a05455b859
CLQueiroz <ceciliaqueiroz@usp.br>
2025-06-03 19:04:42 -0300

    dia 2

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 
-const button = document.getElementById("nome");
-
-const expression = document.getElementById()
+const button = document.getElementById("busca");
+let dados;
+let nome;
+let plataforma;
+let id;
 
 button.onclick= async function buscar(){
 
-const response= await fetch('https://api.rawg.io/api/games?key=1175c03391d84eaf9b022713f3c5e618&search=${expression}')
+const expression = document.getElementById("nome").value;
+const response= await fetch(`https://api.rawg.io/api/games?key=1175c03391d84eaf9b022713f3c5e618&search=${expression}`)
 
-const dados= await response.json();
-console.log (dados);
+dados= await response.json();
+
+    dados.results.forEach(result=>{
+        nome=result.name;
+        console.log (nome);
+
+        id= result.id;
+        console.log(id);
+
+        result.platforms.forEach((item)=>{
+            if(!item.platform)
+                console.log("no games found");
+            plataforma= item.platform.name;
+            console.log(plataforma);
+
+});
+    console.log("---");
+});
+
 }
-
-
 
